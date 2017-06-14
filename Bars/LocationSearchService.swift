e4d1c0dd927bacdcd20ab4344316c11de0b198d7
Mariano Latorre <developer01@C02MD0NUFD58.local>
2017-06-14 17:22:45 +0100

    nothing

--- Bars/LocationSearchService.swift
+++ Bars/LocationSearchService.swift
@@ -108,8 +108,6 @@ class LocationSearchService : LocationSearchable, InternetReachable {
         }.resume()
     }
     
-
-    
     /*
      Sets up the URL object, example:
          https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=YOUR_API_KEY
